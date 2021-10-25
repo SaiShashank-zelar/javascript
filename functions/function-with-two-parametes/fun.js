14b8a31de11b65467e262d428fdad7325d8ddfd5
SaiShashank-zelar <shashank@zelarsoft.com>
2021-10-25 15:41:31 +0530

    change

---
+++ functions/function-with-two-parametes/fun.js
@@ -0,0 +1,6 @@
+/* function with two parameters */
+function run(one,two,four)
+{
+  /*document.write(one + " is better than " + two + "</br>");*/
+   alert(one + " is better than " + two +" this is third fun " +four);
+}
